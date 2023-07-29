0edc12c1512b704a10c49c4e146373c5b6238418
Documenter.jl <documenter@juliadocs.github.io>
2023-07-29 14:45:52 +0000

    build based on 1f2a3be

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MuJoCo","category":"page"},{"location":"#MuJoCo","page":"Home","title":"MuJoCo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MuJoCo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MuJoCo]","category":"page"}]
+}
