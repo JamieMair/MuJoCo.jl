a59204439f0606daeab4d9692300300bcc028e19
nicBarbara <nbar5346@uni.sydney.edu.au>
2023-08-07 18:20:57 -0400

    Created nice visualiser object

--- examples/vis/test_viz.jl
+++ examples/vis/test_viz.jl
@@ -4,77 +4,25 @@ Pkg.activate("..")
 
 using MuJoCo
 
-########################################################################
-# Extras for Lyceum GLFW support
+include("visualiser.jl")
 
-import MuJoCo.LibMuJoCo
-import MuJoCo.LibMuJoCo: mjvScene, mjvCamera, mjvOption, mjvFigure
-import MuJoCo.LibMuJoCo: mjrContext, mjrRect, mjr_render
-
-using GLFW: GLFW, Window, Key, Action, MouseButton, GetKey, RELEASE, PRESS, REPEAT
-using Observables: AbstractObservable, Observable, on, off
-
-const Maybe{T} = Union{T, Nothing}
-
-include("glfw.jl")
-include("utils.jl")
-
-const MAXGEOM = 10000 # preallocated geom array in mjvScene
-
-
-########################################################################
-# Test visualiser: follow recipe from Lyecum Engine type
-
-# Load model and data
-m = load_xml("../../models/humanoid.xml")
-d = init_data(m)
-
-# Create a window and manager
-window = create_window(default_windowsize()..., "MuJoCo.jl")
-mngr = WindowManager(window)
-GLFW.ShowWindow(mngr.state.window)
-
-# Initialise visualisation data structures
-ui = init_ui!(UIState())
-
-# Create scene and context
-# TODO: Should write wrappers so this is nice
-LibMuJoCo.mjv_makeScene(m.internal_pointer, ui.scn, MAXGEOM)
-LibMuJoCo.mjr_makeContext(m.internal_pointer, ui.con, LibMuJoCo.mjFONTSCALE_150)
-
-# TODO: Add equivalent of these once we have nice array handling
-# alignscale!(ui, sim)
-# init_figsensor!(ui.figsensor)
+# Load model, data, and viewer
+model  = load_xml("../../models/humanoid.xml")
+data   = init_data(model)
+viewer = MuJoCoViewer(model, data)
 
 # Loop and simulate for now
-while !GLFW.WindowShouldClose(mngr.state.window)
+while !viewer.should_close
 
     # TODO: nsteps = Int(ceil((1.0/60.0) / m.opt.timestep))
     nsteps = Int(ceil((1.0/60.0) / 0.01))
     for _ in 1:nsteps
-        step!(m, d)
+        step!(model, data)
     end
 
-    # Update scene and render
-    LibMuJoCo.mjv_updateScene(m.internal_pointer, d.internal_pointer, ui.vopt, C_NULL, ui.cam, LibMuJoCo.mjCAT_ALL, ui.scn)
-    render(mngr, ui)
-    GLFW.PollEvents()
+    render!(viewer, model, data)
 end
 
-# Free all the variables
-function close_viewer!(ui,mngr)
-
-    LibMuJoCo.mjv_freeScene(ui.scn)
-    LibMuJoCo.mjr_freeContext(ui.con)
-
-    Libc.free(ui.cam)
-    Libc.free(ui.vopt)
-    Libc.free(ui.scn)
-    Libc.free(ui.con)
-
-    GLFW.DestroyWindow(mngr.state.window)
-end
-
-LibMuJoCo.mj_deleteModel(m.internal_pointer)
-LibMuJoCo.mj_deleteData(d.internal_pointer)
-close_viewer!(ui,mngr)
+LibMuJoCo.mj_deleteModel(model.internal_pointer)
+LibMuJoCo.mj_deleteData(data.internal_pointer)
+close_viewer!(viewer)
---
+++ examples/vis/visualiser.jl
@@ -0,0 +1,144 @@
+import MuJoCo.LibMuJoCo
+import MuJoCo.LibMuJoCo: Model, Data
+import MuJoCo.LibMuJoCo: mjvScene, mjvCamera, mjvOption, mjvFigure
+import MuJoCo.LibMuJoCo: mjrContext, mjrRect, mjr_render
+
+using GLFW: GLFW, Window, Key, Action, MouseButton, GetKey, RELEASE, PRESS, REPEAT
+using Observables: AbstractObservable, Observable, on, off
+
+const Maybe{T} = Union{T, Nothing} # From LyceumBase.jl
+const MAXGEOM = 10000 # preallocated geom array in mjvScene
+
+include("glfw.jl")
+
+# ----------------------------------------------------------------------------------
+
+function alloc(::Type{T}) where {T}
+    return Ptr{T}(Libc.malloc(sizeof(T)))
+end
+
+# From Lyceum types.jl
+Base.@kwdef mutable struct UIState
+    scn::Ptr{mjvScene} = alloc(mjvScene)
+    cam::Ptr{mjvCamera} = alloc(mjvCamera)
+    vopt::Ptr{mjvOption} = alloc(mjvOption)
+    con::Ptr{mjrContext} = alloc(mjrContext)
+    figsensor::Ptr{mjvFigure} = alloc(mjvFigure)
+
+    showinfo::Bool = true
+    showsensor::Bool = false
+    speedmode::Bool = false
+    speedfactor::Float64 = 1 / 10
+
+    reversed::Bool = false
+    paused::Bool = true
+    shouldexit::Bool = false
+
+    reward::Float64 = 0
+
+    lastrender::Float64 = 0
+    refreshrate::Float64 = 0
+    realtimerate::Float64 = 0
+    io1::IOBuffer = IOBuffer()
+    io2::IOBuffer = IOBuffer()
+
+    lock::ReentrantLock = ReentrantLock()
+end
+
+"""
+    init_ui!(ui::UIState)
+
+Initialise an instance of `UIState` with defaults
+"""
+function init_ui!(ui::UIState)
+    LibMuJoCo.mjv_defaultScene(ui.scn)
+    LibMuJoCo.mjv_defaultCamera(ui.cam)
+    LibMuJoCo.mjv_defaultOption(ui.vopt)
+    LibMuJoCo.mjr_defaultContext(ui.con)
+    LibMuJoCo.mjv_defaultFigure(ui.figsensor)
+    return ui
+end
+
+# Adapted from LyceumMuJoCoViz.jl
+function render(manager::WindowManager, ui::UIState)
+    w, h = GLFW.GetFramebufferSize(manager.state.window)
+    rect = mjrRect(Cint(0), Cint(0), Cint(w), Cint(h))
+    mjr_render(rect, ui.scn, ui.con)
+    # ui.showinfo && overlay_info(rect, e) # TODO: Add in the info later
+    GLFW.SwapBuffers(manager.state.window)
+    return
+end
+
+mutable struct MuJoCoViewer
+    manager::WindowManager
+    ui::UIState
+    should_close::Bool
+end
+
+"""
+    MuJoCoViewer(m::Model, d::Data; show_window=true)
+
+Initialise a visualiser for a given MuJoCo model
+"""
+function MuJoCoViewer(m::Model, d::Data; show_window=true)
+
+    # Create and show window
+    window = create_window(default_windowsize()..., "MuJoCo.jl")
+    manager = WindowManager(window)
+    show_window && GLFW.ShowWindow(manager.state.window)
+
+    # Initialise visualisation data structures
+    ui = init_ui!(UIState())
+
+    # Create scene and context
+    LibMuJoCo.mjv_makeScene(m.internal_pointer, ui.scn, MAXGEOM)
+    LibMuJoCo.mjr_makeContext(m.internal_pointer, ui.con, LibMuJoCo.mjFONTSCALE_150)
+
+    # TODO: Add equivalent of these once we have nice array handling
+    # alignscale!(ui, sim)
+    # init_figsensor!(ui.figsensor)
+
+    return MuJoCoViewer(manager, ui, false)
+end
+
+"""
+    render!(viewer::MuJoCoViewer, m::Model, d::Data)
+
+Render the viewer given current model and data.
+"""
+function render!(viewer::MuJoCoViewer, m::Model, d::Data)
+    LibMuJoCo.mjv_updateScene(
+        m.internal_pointer, 
+        d.internal_pointer, 
+        viewer.ui.vopt, 
+        C_NULL, 
+        viewer.ui.cam, 
+        LibMuJoCo.mjCAT_ALL, 
+        viewer.ui.scn
+    )
+    render(viewer.manager, viewer.ui)
+    GLFW.PollEvents()
+    viewer.should_close = GLFW.WindowShouldClose(viewer.manager.state.window)
+
+    return nothing
+end
+
+"""
+    close_viewer!(ui, manager)
+
+Close the viewer when done.
+"""
+function close_viewer!(viewer::MuJoCoViewer)
+
+    LibMuJoCo.mjv_freeScene(viewer.ui.scn)
+    LibMuJoCo.mjr_freeContext(viewer.ui.con)
+
+    Libc.free(viewer.ui.cam)
+    Libc.free(viewer.ui.vopt)
+    Libc.free(viewer.ui.scn)
+    Libc.free(viewer.ui.con)
+
+    GLFW.DestroyWindow(viewer.manager.state.window)
+
+    return nothing
+end
