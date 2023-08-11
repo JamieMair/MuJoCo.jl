d26e922558724ae82b63b8ed529c355f9b2f7d17
Jamie Mair <JamieMair@users.noreply.github.com>
2023-08-11 07:50:11 +0100

    Removed main, but fixed relative paths

--- examples/vis/test_vis.jl
+++ examples/vis/test_vis.jl
@@ -1,39 +1,32 @@
-cd(@__DIR__)
-using Pkg
-Pkg.activate("..")
-
 using MuJoCo
-
 include("visualiser.jl")
 
 
-function main()
-    # Load model, data, and viewer
-    model  = load_xml("../../models/cartpole.xml")
-    data   = init_data(model)
-    viewer = MuJoCoViewer(model, data)
+root_path = normpath(joinpath(@__DIR__, "..", ".."))
+# Load model, data, and viewer
+model  = load_xml(joinpath(root_path, "models", "cartpole.xml"))
+data   = init_data(model)
+viewer = MuJoCoViewer(model, data)
 
-    # Change initial conditions
-    d.qpos .= [0, 0.1]
+# Change initial conditions
+d.qpos .= [0, 0.1]
 
-    # Loop and simulate for now
-    while !viewer.should_close
 
-        # TODO: Throttle visualisation inside render!() somewhere
-        nsteps = Int(ceil((1.0/60.0) / m.opt.timestep))
-        for _ in 1:nsteps
-            step!(model, data)
-        end
+# Loop and simulate for now
+while !viewer.should_close
 
-        render!(viewer, model, data)
+    # TODO: Throttle visualisation inside render!() somewhere
+    nsteps = Int(ceil((1.0/60.0) / m.opt.timestep))
+    for _ in 1:nsteps
+        step!(model, data)
     end
 
-    LibMuJoCo.mj_deleteModel(model.internal_pointer)
-    LibMuJoCo.mj_deleteData(data.internal_pointer)
-    close_viewer!(viewer)
+    render!(viewer, model, data)
 end
 
-main()
+LibMuJoCo.mj_deleteModel(model.internal_pointer)
+LibMuJoCo.mj_deleteData(data.internal_pointer)
+close_viewer!(viewer)
 
 """
 Next steps: get mouse and button callbacks working
