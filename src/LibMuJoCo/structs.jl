9db2647e27f394490bdd4f1e84992fddd48bb136
nicBarbara <nbar5346@uni.sydney.edu.au>
2023-11-21 11:55:46 +1100

    Added note on MuJoCo IDs starting at 0 to named access

--- src/LibMuJoCo/structs.jl
+++ src/LibMuJoCo/structs.jl
@@ -1125,7 +1125,7 @@ struct mjOption_
     )
 end
 const mjOption = mjOption_
-struct var"##Ctag#314"
+struct var"##Ctag#304"
     fovy::Cfloat
     ipd::Cfloat
     azimuth::Cfloat
@@ -1136,8 +1136,8 @@ struct var"##Ctag#314"
     offwidth::Cint
     offheight::Cint
     ellipsoidinertia::Cint
-    var"##Ctag#314"() = new()
-    var"##Ctag#314"(
+    var"##Ctag#304"() = new()
+    var"##Ctag#304"(
         fovy::Cfloat,
         ipd::Cfloat,
         azimuth::Cfloat,
@@ -1161,14 +1161,14 @@ struct var"##Ctag#314"
         ellipsoidinertia,
     )
 end
-struct var"##Ctag#315"
+struct var"##Ctag#305"
     shadowsize::Cint
     offsamples::Cint
     numslices::Cint
     numstacks::Cint
     numquads::Cint
-    var"##Ctag#315"() = new()
-    var"##Ctag#315"(
+    var"##Ctag#305"() = new()
+    var"##Ctag#305"(
         shadowsize::Cint,
         offsamples::Cint,
         numslices::Cint,
@@ -1176,20 +1176,20 @@ struct var"##Ctag#315"
         numquads::Cint,
     ) = new(shadowsize, offsamples, numslices, numstacks, numquads)
 end
-struct var"##Ctag#316"
+struct var"##Ctag#306"
     ambient::NTuple{3,Cfloat}
     diffuse::NTuple{3,Cfloat}
     specular::NTuple{3,Cfloat}
     active::Cint
-    var"##Ctag#316"() = new()
-    var"##Ctag#316"(
+    var"##Ctag#306"() = new()
+    var"##Ctag#306"(
         ambient::NTuple{3,Cfloat},
         diffuse::NTuple{3,Cfloat},
         specular::NTuple{3,Cfloat},
         active::Cint,
     ) = new(ambient, diffuse, specular, active)
 end
-struct var"##Ctag#317"
+struct var"##Ctag#307"
     stiffness::Cfloat
     stiffnessrot::Cfloat
     force::Cfloat
@@ -1203,8 +1203,8 @@ struct var"##Ctag#317"
     shadowclip::Cfloat
     shadowscale::Cfloat
     actuatortendon::Cfloat
-    var"##Ctag#317"() = new()
-    var"##Ctag#317"(
+    var"##Ctag#307"() = new()
+    var"##Ctag#307"(
         stiffness::Cfloat,
         stiffnessrot::Cfloat,
         force::Cfloat,
@@ -1234,7 +1234,7 @@ struct var"##Ctag#317"
         actuatortendon,
     )
 end
-struct var"##Ctag#318"
+struct var"##Ctag#308"
     forcewidth::Cfloat
     contactwidth::Cfloat
     contactheight::Cfloat
@@ -1251,8 +1251,8 @@ struct var"##Ctag#318"
     framewidth::Cfloat
     constraint::Cfloat
     slidercrank::Cfloat
-    var"##Ctag#318"() = new()
-    var"##Ctag#318"(
+    var"##Ctag#308"() = new()
+    var"##Ctag#308"(
         forcewidth::Cfloat,
         contactwidth::Cfloat,
         contactheight::Cfloat,
@@ -1288,7 +1288,7 @@ struct var"##Ctag#318"
         slidercrank,
     )
 end
-struct var"##Ctag#319"
+struct var"##Ctag#309"
     fog::NTuple{4,Cfloat}
     haze::NTuple{4,Cfloat}
     force::NTuple{4,Cfloat}
@@ -1311,8 +1311,8 @@ struct var"##Ctag#319"
     constraint::NTuple{4,Cfloat}
     slidercrank::NTuple{4,Cfloat}
     crankbroken::NTuple{4,Cfloat}
-    var"##Ctag#319"() = new()
-    var"##Ctag#319"(
+    var"##Ctag#309"() = new()
+    var"##Ctag#309"(
         fog::NTuple{4,Cfloat},
         haze::NTuple{4,Cfloat},
         force::NTuple{4,Cfloat},
@@ -4983,7 +4983,7 @@ struct mjvFigure_
     )
 end
 const mjvFigure = mjvFigure_
-struct var"##Ctag#320"
+struct var"##Ctag#310"
     nu::Cint
     na::Cint
     nbody::Cint
@@ -5125,8 +5125,8 @@ struct var"##Ctag#320"
     name_tendonadr::Ptr{Cint}
     name_actuatoradr::Ptr{Cint}
     names::Ptr{Cchar}
-    var"##Ctag#320"() = new()
-    var"##Ctag#320"(
+    var"##Ctag#310"() = new()
+    var"##Ctag#310"(
         nu::Cint,
         na::Cint,
         nbody::Cint,
@@ -5412,7 +5412,7 @@ struct var"##Ctag#320"
         names,
     )
 end
-struct var"##Ctag#321"
+struct var"##Ctag#311"
     warning::NTuple{8,mjWarningStat}
     nefc::Cint
     ncon::Cint
@@ -5444,8 +5444,8 @@ struct var"##Ctag#321"
     bvh_active::Ptr{mjtByte}
     contact::Ptr{mjContact}
     efc_force::Ptr{mjtNum}
-    var"##Ctag#321"() = new()
-    var"##Ctag#321"(
+    var"##Ctag#311"() = new()
+    var"##Ctag#311"(
         warning::NTuple{8,mjWarningStat},
         nefc::Cint,
         ncon::Cint,
